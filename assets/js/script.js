7273f085de6e5456a0f6704e0edb61074c1bc09d
Rachelz85 <darthrachelz@gmail.com>
2024-05-05 13:11:32 +0000

    Add images array to the js file

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const images = [
+    { src: 'assets/images/cap.jpg', name: 'captain america', alt: 'Captain America' }, 
+    { src: 'assets/images/cap.jpg', name: 'captain america', alt: 'Captain America' },
+    { src: 'assets/images/deadpool.jpg', name: 'deadpool', alt: 'Deadpool' },
+    { src: 'assets/images/deadpool.jpg', name: 'deadpool', alt: 'Deadpool' },
+    { src: 'assets/images/hulk.jpg', name: 'hulk', alt: 'The Hulk' },
+    { src: 'assets/images/hulk.jpg', name: 'hulk', alt: 'The Hulk' },
+    { src: 'assets/images/ironman.jpg', name: 'iron man', alt: 'Iron Man' },
+    { src: 'assets/images/ironman.jpg', name: 'iron man', alt: 'Iron Man' },
+    { src: 'assets/images/spiderman.jpg', name: 'spiderman', alt: 'Spider-Man' },
+    { src: 'assets/images/spiderman.jpg', name: 'spiderman', alt: 'Spider-Man' },
+    { src: 'assets/images/spiderwoman.jpg', name: 'spiderwoman', alt: 'Spider-Woman' },
+    { src: 'assets/images/spiderwoman.jpg', name: 'spiderwoman', alt: 'Spider-Woman' },
+    { src: 'assets/images/suestorm.jpg', name: 'suestorm', alt: 'Sue Storm' },
+    { src: 'assets/images/suestorm.jpg', name: 'suestorm', alt: 'Sue Storm' },
+    { src: 'assets/images/wolverine.jpg', name: 'wolverine', alt: 'Wolverine' },
+    { src: 'assets/images/wolverine.jpg', name: 'wolverine', alt: 'Wolverine' }
+]
